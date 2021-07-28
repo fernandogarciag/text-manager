a6ab1d754e1b3fcbd45dc407bf843eadee02b2a5
Fernando García <fernandgarcia97@hotmail.com>
2021-07-28 10:16:55 -0500

    add undo

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,12 @@ import { Container } from "./styles";
 
 function App() {
   const [list, setList] = useState([]);
+  const [undoList, setUndoList] = useState([]);
   const addElement = (e) => {
     e.preventDefault();
     const newValue = prompt("Nuevo valor para la cadena");
     if (newValue) {
+      checkPoint();
       setList([
         ...list,
         newValue
@@ -14,24 +16,44 @@ function App() {
     }
   }
   const deleteElement = (e) => {
-    e.preventDefault();
     let listCopy = [...list];
+    checkPoint();
+    listCopy.splice(e.target.value,1);
+    setList([...listCopy]);
+  }
+  const deleteElements = (e) => {
+    e.preventDefault();
     const select = document.getElementById('element');
     if (Object.entries(select.selectedOptions).length) {
+      checkPoint();
+      let listCopy = [...list];
       for (let index = Object.entries(select.selectedOptions).length - 1; index >= 0; index--) {
         listCopy.splice(Object.entries(select.selectedOptions)[index][1].value,1);
       }
       setList([...listCopy]);
     }
   }
+  const checkPoint = () => {
+    let listCopy = [...undoList];
+    listCopy.push([...list]);
+    setUndoList([...listCopy]);
+  }
+  const undoElement = () => {
+    let listCopy = [...undoList];
+    setList([...listCopy.pop()]);
+    setUndoList([...listCopy]);
+  }
   return (
     <Container>
       <select id="element" name="element" multiple>
-        {list.map((element, index) => (<option value={index} key={index}>{element}</option>))}
+        {list.map((element, index) => (
+        <option value={index} key={index} onDoubleClick={(e) => deleteElement(e)}>{element}</option>
+        ))}
       </select>
-      <div classname="buttons">
+      <div className="buttons">
       <button onClick={(e) => addElement(e)}> + </button>
-      <button onClick={(e) => deleteElement(e)}> - </button>
+      <button onClick={(e) => deleteElements(e)}> - </button>
+      <button onClick={(e) => undoElement()}> {"<="} </button>
       </div>
     </Container>
   );
